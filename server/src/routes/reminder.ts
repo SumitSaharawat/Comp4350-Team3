745573312a9503627f07a85497e258083332f7e0
Han Sun <sunh9@myumanitoba.ca>
2025-03-09 00:56:30 -0600

    Add reminder.ts

---
+++ server/src/routes/reminder.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { 
+    addReminderController, 
+    getAllRemindersController, 
+    editReminderController, 
+    deleteReminderController 
+} from '../controller/reminderController';
+import { authenticateToken } from '../middleware/authenticator';
+import { validateReminderRequest, validateParams } from '../middleware/dbValidation';
+
+const router = express.Router();
+
+router.get('/:userId', authenticateToken, validateReminderRequest, validateParams('userId'), getAllRemindersController);
+router.post('/', authenticateToken, validateReminderRequest, addReminderController);
+router.put('/:id', authenticateToken, validateReminderRequest, validateParams('id'), editReminderController);
+router.delete('/:id', authenticateToken, validateReminderRequest, validateParams('id'), deleteReminderController);
+
+export default router;

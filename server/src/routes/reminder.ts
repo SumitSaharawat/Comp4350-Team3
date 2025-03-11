d560c17dbd7c04eb4f80edae7f064da792cfcd5d
Han Sun <sunh9@myumanitoba.ca>
2025-03-11 10:26:17 -0500

    Change description

--- server/src/routes/reminder.ts
+++ server/src/routes/reminder.ts
@@ -9,18 +9,27 @@ import { authenticateToken } from '../middleware/authenticator';
 import { validateReminderRequest, validateParams } from '../middleware/dbValidation';
 
 const router = express.Router();
+
 // http://localhost:8000/api/reminder/userId
 router.get('/:userId', authenticateToken, validateReminderRequest,validateParams('userId'), getAllRemindersController);
+
 //example body：
 //{
 //    "user": "67c27af37862793900fc11b9", 
-//    "name": "Meeting Reminder", 
-//    "text": "You have a meeting at 3 PM", 
+//    "name": "Pay Reminder", 
+//    "text": "You have to pay at 3 PM", 
 //    "time": "2025-03-10T15:00:00Z"
 //  }
 router.post('/', authenticateToken, validateReminderRequest, addReminderController);
 
 // http://localhost:8000/api/reminder/reminderId
+//example body：
+//{
+//    "user": "67c27af37862793900fc11b9", 
+//    "name": "Change Payment", 
+//    "text": "You have to pay at 1 PM", 
+//    "time": "2025-03-10T15:00:00Z"
+//  }
 router.put('/:id', authenticateToken, validateReminderRequest, validateParams('id'), editReminderController);
 // http://localhost:8000/api/reminder/reminderId
 router.delete('/:id', authenticateToken, validateReminderRequest, validateParams('id'), deleteReminderController);

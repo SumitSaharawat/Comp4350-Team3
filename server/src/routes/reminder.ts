36a0af8c58c72c305b6ee2d3d76bf15e18f86960
Han Sun <sunh9@myumanitoba.ca>
2025-03-10 22:05:23 -0500

    Change all "user" to "userId"

--- server/src/routes/reminder.ts
+++ server/src/routes/reminder.ts
@@ -9,10 +9,20 @@ import { authenticateToken } from '../middleware/authenticator';
 import { validateReminderRequest, validateParams } from '../middleware/dbValidation';
 
 const router = express.Router();
-
+// http://localhost:8000/api/reminder/userId
 router.get('/:userId', authenticateToken, validateParams('userId'), getAllRemindersController);
+//example body：
+//{
+//    "user": "67c27af37862793900fc11b9", 
+//    "name": "Meeting Reminder", 
+//    "text": "You have a meeting at 3 PM", 
+//    "time": "2025-03-10T15:00:00Z"
+//  }
 router.post('/', authenticateToken, validateReminderRequest, addReminderController);
+
+// http://localhost:8000/api/reminder/reminderId
 router.put('/:id', authenticateToken, validateReminderRequest, validateParams('id'), editReminderController);
+// http://localhost:8000/api/reminder/reminderId
 router.delete('/:id', authenticateToken, validateReminderRequest, validateParams('id'), deleteReminderController);
 
 export default router;

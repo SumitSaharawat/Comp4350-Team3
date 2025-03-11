8cc8976b1f78e6804c578ae0b77edef33c39e569
Han Sun <sunh9@myumanitoba.ca>
2025-03-10 20:03:33 -0500

    fix minor error for reminder.ts

--- server/src/routes/reminder.ts
+++ server/src/routes/reminder.ts
@@ -10,7 +10,7 @@ import { validateReminderRequest, validateParams } from '../middleware/dbValidat
 
 const router = express.Router();
 
-router.get('/:userId', authenticateToken, validateReminderRequest, validateParams('userId'), getAllRemindersController);
+router.get('/:userId', authenticateToken, validateParams('userId'), getAllRemindersController);
 router.post('/', authenticateToken, validateReminderRequest, addReminderController);
 router.put('/:id', authenticateToken, validateReminderRequest, validateParams('id'), editReminderController);
 router.delete('/:id', authenticateToken, validateReminderRequest, validateParams('id'), deleteReminderController);

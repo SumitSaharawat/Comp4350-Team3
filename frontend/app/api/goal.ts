394d4308eca75689dce1def53b601d8b5add76dd
xiangshi <shix2@myumanitoba.ca>
2025-03-11 20:17:32 -0500

    add goal api call

--- frontend/app/api/goal.ts
+++ frontend/app/api/goal.ts
@@ -21,3 +21,25 @@ export async function getGoalsFromServer(userId: string): Promise<Goal[]> {
     const data = await res.json();
     return data || [];
 }
+
+export async function addGoalToServer(
+    userId: string,
+    name: string,
+    time: string,
+    currAmount: number,
+    goalAmount: number,
+    category: string): Promise< {message: string} > {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/goal/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId, name, time, currAmount, goalAmount, category}),
+        credentials: "include"
+    });
+
+    if (!res.ok) {
+        throw new Error((await res.json()).message || "Failed to add goal");
+    }
+
+    return res.json();
+}

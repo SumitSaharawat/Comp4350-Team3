7782916ce551792d0bebf76e19fbc54860eaff44
xiangshi <shix2@myumanitoba.ca>
2025-03-26 11:46:22 -0500

    finished integrate tag into transaction page

--- frontend/components/ui/TransactionFormModal.tsx
+++ frontend/components/ui/TransactionFormModal.tsx
@@ -9,11 +9,13 @@ import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { X } from "lucide-react";
+import { useTags } from "@/app/contexts/TagContext";
 import {
     addTransactionsToServer,
     editTransactionsOnServer,
     Transaction,
 } from "@/app/api/transac";
+import {Tag} from "@/app/api/tag";
 const currencies = ["CAD", "USD"];
 const types = ["Saving", "Spending"];
 
@@ -32,12 +34,14 @@ export default function TransactionFormModal({
     mode,
     existingTransaction,
 }: TransactionFormModalProps) {
+    const { tags, getTags } = useTags();
     const [transacData, setTransacData] = useState({
         name: "",
         amount: null as number | null,
         time: new Date(),
         type: "Saving",
         currency: "CAD",
+        tags: [] as Tag[],
     });
 
     const [loading, setLoading] = useState(false);
@@ -73,12 +77,39 @@ export default function TransactionFormModal({
             transacData.time = new Date(existingTransaction.date);
             transacData.type = existingTransaction.type;
             transacData.currency = existingTransaction.currency;
+            transacData.tags = existingTransaction.tags || [];
             setHeadLine("Edit Transaction");
         } else {
             cleanTransacData();
             setHeadLine("Add Transaction");
         }
     }, [mode, existingTransaction]);
+
+
+    useEffect(() => {
+        const fetchTags = async () => {
+            const userId = localStorage.getItem("userid");
+            if (userId) {
+                await getTags(userId);
+            }
+        };
+
+        if (isOpen) {
+            fetchTags();
+        }
+    }, [isOpen, getTags]);
+
+    const handleTagSelection = (tag: Tag) => {
+        setTransacData((prev) => {
+            const isTagSelected = prev.tags.some((t) => t.id === tag.id);
+
+            const newTags = isTagSelected
+                ? prev.tags.filter((t) => t.id !== tag.id)
+                : [...prev.tags, tag];
+
+            return { ...prev, tags: newTags };
+        });
+    };
 
     const handleSubmit = async () => {
         setMessage(null);
@@ -117,7 +148,8 @@ export default function TransactionFormModal({
                     formattedDate,
                     transacData.amount,
                     transacData.type,
-                    transacData.currency
+                    transacData.currency,
+                    transacData.tags,
                 );
                 setMessage({
                     text: "Transaction added successfully!",
@@ -130,7 +162,8 @@ export default function TransactionFormModal({
                     formattedDate,
                     transacData.amount,
                     transacData.type,
-                    transacData.currency
+                    transacData.currency,
+                    transacData.tags,
                 );
                 setMessage({
                     text: "Transaction updated successfully!",
@@ -238,6 +271,36 @@ export default function TransactionFormModal({
                     className="w-full border border-gray-300 p-2 rounded mb-2"
                     showPopperArrow={false}
                 />
+
+                {/* Tags Selection */}
+                <div className="mb-4">
+                    <label className="font-bold mb-2 block">Tags</label>
+                    <div className="flex flex-wrap gap-2 mt-2">
+                        {tags.map((tag) => {
+                            const isTagSelected = transacData.tags.some((t) => t.id === tag.id);
+                            return (
+                                <div
+                                    key={tag.id}
+                                    className={`flex items-center gap-1 p-1 rounded ${isTagSelected ? "bg-blue-100" : ""}`}
+                                >
+                                    <input
+                                        type="checkbox"
+                                        checked={isTagSelected}
+                                        onChange={() => handleTagSelection(tag)}
+                                        className="mr-1 cursor-pointer"
+                                    />
+                                    <span
+                                        className="px-2 py-1 text-sm rounded-full cursor-pointer"
+                                        style={{ backgroundColor: tag.color, color: "white" }}
+                                        onClick={() => handleTagSelection(tag)}
+                                    >
+                                        {tag.name}
+                                    </span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
 
                 {/* Save Button */}
                 <button
--- frontend/components/ui/TransactionList.tsx
+++ frontend/components/ui/TransactionList.tsx
@@ -35,100 +35,117 @@ export default function TransactionList({
         <div className="w-full h-full overflow-y-auto border border-gray-300 rounded-lg shadow-sm">
             <table className="w-full border-collapse table-fixed">
                 <thead className="sticky top-0 bg-gray-100 text-black">
-                    <tr>
-                        <th className="border-b border-gray-400 px-2 py-2 font-bold text-left">
-                            Name
-                        </th>
-                        <th className="border-b border-gray-400 px-3 py-2 font-bold text-left">
-                            Date
-                        </th>
-                        <th className="border-b border-gray-400 px-4 py-2 font-bold text-left">
-                            Amount
-                        </th>
-                        <th className="border-b border-gray-400 px-4 py-2 font-bold text-left">
-                            Type
-                        </th>
-                        <th className="border-b border-gray-400 px-4 py-2 font-bold text-left">
-                            Currency
-                        </th>
-                        <th className="border-b border-gray-400 px-4 py-2 font-bold text-center">
-                            Actions
-                        </th>
-                    </tr>
+                <tr>
+                    <th className="border-b border-gray-400 px-2 py-2 font-bold text-left">
+                        Name
+                    </th>
+                    <th className="border-b border-gray-400 px-3 py-2 font-bold text-left">
+                        Date
+                    </th>
+                    <th className="border-b border-gray-400 px-3 py-2 font-bold text-left">
+                        Amount
+                    </th>
+                    <th className="border-b border-gray-400 px-3 py-2 font-bold text-left">
+                        Type
+                    </th>
+                    <th className="border-b border-gray-400 px-3 py-2 font-bold text-left">
+                        Currency
+                    </th>
+                    <th className="border-b border-gray-400 py-2 font-bold text-center">
+                        Tags
+                    </th>
+                    <th className="border-b border-gray-400 pr-2 pl-4 py-2 font-bold text-center">
+                        Actions
+                    </th>
+                </tr>
                 </thead>
 
                 <tbody className="text-black">
-                    {transactions.length > 0 ? (
-                        transactions.map((tx) => (
-                            <React.Fragment key={tx.id}>
-                                {/* Transaction Row */}
-                                <tr className="hover:bg-gray-200">
-                                    <td className="border-b border-gray-400 px-2 py-2 text-left">
-                                        {tx.name || ""}
-                                    </td>
-                                    <td className="border-b border-gray-400 px-3 py-2 text-left">
-                                        {new Date(tx.date).toLocaleDateString()}
-                                    </td>
-                                    <td className="border-b border-gray-400 px-4 py-2 text-left">
-                                        {tx.amount.toFixed(2)}
-                                    </td>
-                                    <td className="border-b border-gray-400 px-4 py-2 text-left">
-                                        {tx.type}
-                                    </td>
-                                    <td className="border-b border-gray-400 px-4 py-2 text-left">
-                                        {tx.currency}
-                                    </td>
-                                    {/* Toggle Actions Button */}
-                                    <td className="border-b border-gray-400 px-4 py-2 text-center">
-                                        <button
-                                            onClick={() => toggleRow(tx.id)}
-                                            className="text-gray-600 hover:text-black"
-                                        >
-                                            <MoreHorizontal size={20} />
-                                        </button>
+                {transactions.length > 0 ? (
+                    transactions.map((tx) => (
+                        <React.Fragment key={tx.id}>
+                            {/* Transaction Row */}
+                            <tr className="hover:bg-gray-200 h-12">
+                                <td className="border-b border-gray-400 px-2 py-2 text-left">
+                                    {tx.name || ""}
+                                </td>
+                                <td className="border-b border-gray-400 px-3 py-2 text-left">
+                                    {new Date(tx.date).toLocaleDateString()}
+                                </td>
+                                <td className="border-b border-gray-400 px-3 py-2 text-left">
+                                    {tx.amount.toFixed(2)}
+                                </td>
+                                <td className="border-b border-gray-400 px-3 py-2 text-left">
+                                    {tx.type}
+                                </td>
+                                <td className="border-b border-gray-400 px-3 py-2 text-left">
+                                    {tx.currency}
+                                </td>
+                                <td className="border-b border-gray-400 py-2 text-left">
+                                    <div className="flex items-center gap-2 flex-wrap">
+                                        {tx.tags.map((tag: { id: string; name: string; color: string }) => (
+                                            <div key={tag.id} className="flex items-center gap-1">
+                                               <span
+                                                   className="px-3 py-1 text-white rounded-full text-sm font-medium inline-block"
+                                                   style={{backgroundColor: tag.color}}
+                                               >
+                                                    {tag.name}
+                                                </span>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </td>
+                                {/* Toggle Actions Button */}
+                                <td className="border-b border-gray-400 pr-2 pl-4 py-2 text-center">
+                                    <button
+                                        onClick={() => toggleRow(tx.id)}
+                                        className="text-gray-600 hover:text-black"
+                                    >
+                                        <MoreHorizontal size={20}/>
+                                    </button>
+                                </td>
+                            </tr>
+
+                            {/* Hidden Action Row (Appears on Toggle) */}
+                            {expandedRow === tx.id && (
+                                <tr>
+                                    <td
+                                        colSpan={7}
+                                        className="border-b border-gray-400 p-2 text-center"
+                                    >
+                                        <div className="flex justify-center gap-4">
+                                            <button
+                                                onClick={() =>
+                                                    handleDelete(tx.id)
+                                                }
+                                                className="text-red-600 hover:text-red-800 flex items-center gap-1"
+                                            >
+                                                <Trash2 size={18} />
+                                                Delete
+                                            </button>
+                                            <button
+                                                onClick={() => onEdit(tx)}
+                                                className="text-blue-600 hover:text-blue-800 flex items-center gap-1"
+                                            >
+                                                <Edit2 size={18} />
+                                                Edit
+                                            </button>
+                                        </div>
                                     </td>
                                 </tr>
-
-                                {/* Hidden Action Row (Appears on Toggle) */}
-                                {expandedRow === tx.id && (
-                                    <tr>
-                                        <td
-                                            colSpan={6}
-                                            className="border-b border-gray-400 p-2 text-center"
-                                        >
-                                            <div className="flex justify-center gap-4">
-                                                <button
-                                                    onClick={() =>
-                                                        handleDelete(tx.id)
-                                                    }
-                                                    className="text-red-600 hover:text-red-800 flex items-center gap-1"
-                                                >
-                                                    <Trash2 size={18} />
-                                                    Delete
-                                                </button>
-                                                <button
-                                                    onClick={() => onEdit(tx)}
-                                                    className="text-blue-600 hover:text-blue-800 flex items-center gap-1"
-                                                >
-                                                    <Edit2 size={18} />
-                                                    Edit
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )}
-                            </React.Fragment>
-                        ))
-                    ) : (
-                        <tr>
-                            <td
-                                colSpan={6}
-                                className="text-center text-gray-500 py-4"
-                            >
-                                No transactions found.
-                            </td>
-                        </tr>
-                    )}
+                            )}
+                        </React.Fragment>
+                    ))
+                ) : (
+                    <tr>
+                        <td
+                            colSpan={6}
+                            className="text-center text-gray-500 py-4"
+                        >
+                            No transactions found.
+                        </td>
+                    </tr>
+                )}
                 </tbody>
             </table>
         </div>
